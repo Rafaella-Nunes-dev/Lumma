9703a68809ec568a56d2d098c6795596a8993e67
Rafaella Nunes <rafaella11garcia@gmail.com>
2025-12-02 21:23:48 -0300

    Pq não vai??

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+/*const container = document.getElementById("catalogo");
+
+var itens = [];
+
+function CriaCards()
+{
+  fetch("catalogo.json")
+    .then(response => response.json())
+    .then(data => {
+      //const container = document.getElementById("catalogo");
+  
+      data.velas.forEach(vela => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+          <img src="${vela.imagem}" alt="${vela.nome}">
+          <h3>${vela.nome}</h3>
+          <p>${vela.descricao}</p>
+          <p><strong>R$ ${vela.preco.toFixed(2)}</strong></p>
+          <button>Comprar</button>
+        `;
+        container.appendChild(card);
+
+        itens.push(vela);
+        console.log(itens);
+      });
+    });
+}
+CriaCards();
+  //tentativa de barra de pesquisa
+
+
+
+
+document.getElementById("searchBar").addEventListener("blur", pesquisar);
+
+function pesquisar()
+{
+  var input = document.getElementById("searchBar").value;
+  console.log("input= ", input);
+  container.innerHTML = "";
+
+  var itens_pesquisa = itens.filter(function(itens) {
+    return 
+  });
+  itens_pesquisa.forEach((itens_pesquisa) => CriaCards);
+}*/
+
 
 fetch("catalogo.json")
   .then(response => response.json())
@@ -17,7 +65,7 @@ fetch("catalogo.json")
           <h3>${vela.nome}</h3>
           <p>${vela.descricao}</p>
           <p><strong>R$ ${vela.preco.toFixed(2)}</strong></p>
-          <a href="https://wa.me/988302496"><button>Comprar</button></a>
+          <a  href="https://wa.me/988302496" ><button>Comprar</button></a>
         `;
         container.appendChild(card);
         });
@@ -36,7 +84,4 @@ fetch("catalogo.json")
       exibirVelas(resultado);
     });
   })
-
   .catch(error => console.error("Erro ao carregar JSON:", error));
-
-
